ef5ba6d23c3e1e51b9013629aec31d948b7d47f3
unknown <a0919458922@gmail.com>
2025-02-24 17:58:48 +0800

    迎新活動介紹上傳

---
+++ 迎新活動介紹.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>
+            迎新活動介紹
+        </title>
+    </head>
+    <body bgcolor="#E6D6F0">
+        <meta charset = "UTF-8">
+        <h1><B><center>迎新活動介紹</center></B></h1>
+        <hr><BR>
+        <OL>
+            <B><H2><LI>活動時間：2025/02/28~2025/03/02</LI></H2></B>
+            <B><H2><LI>活動地點：<a href ="https://www.sunmoonlake.gov.tw/zh-tw" target = "new">南投日月潭</a></LI></H2></B>
+            <B><H2><LI>活動內容：團隊挑戰活動、抽獎活動、特色美食體驗等等 (細節活動當天會說明)</LI></H2></B>
+            <B><H2><LI>活動費用：每人2500元 (越多人參加越便宜，詳情請見下方表格)</LI></H2></B>
+            <table border="1" width="50%" height="10%" style="font-size: 24px;">
+                <tr><td><center>人數</center></td><td><center>價格</center></td></tr>
+                <tr><td><center><10人</center></td><td><center>2500元/人</center></td></tr>
+                <tr><td><center>10~20人</center></td><td><center>2250元/人</center></td></tr>
+                <tr><td><center>>20人</center></td><td><center>2000元/人</center></td></tr>
+            </table>
+            <B><H2><LI>注意事項</LI></H2></B>
+            <UL><H3><LI>請於<font color= "red"><u>2025/02/25 23:59:59</u></font>前報名完成，逾期不受理</LI></H3></UL>
+            <UL><H3><LI>費用將在報名截止後，透過Email方式告知</LI></H3></UL>
+            <UL><H3><LI>請於<font color= "red"><u>2025/02/27 23:59:59</u></font>前繳費完成，逾期將視為沒有報名成功</LI></H3></UL>
+            <UL><H3><LI>若有任何問題，請聯絡活動負責人：<a href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" target = "new">王小明</a> <br><a href = "mailto:Orientation@mail.nuk.edu.tw">Orientation@mail.nuk.edu.tw</a></LI></H3></UL>
+        </OL>
+        <font size="10"><p><center><b><a href = "迎新活動報名表.php" target = "new">報名連結</a></center></p></b></font>
+        <div style="display: flex;">
+            <img src="https://www.sunmoonlake.gov.tw/zh-tw/Content/images/index/new-8view-01.jpg" width="50%">
+            <img src="https://cja.tw/images/Attractions/around/sunmoonlake-960.jpg" width="50%">
+        </div>
+        <a href = "https://www.youtube.com/watch?v=5a54TPGz9bI" target = "new"><b><center><font size="5"><del><font color= "red">活</font><font color= "orange">動</font><font color= "yellow">宣</font><font color= "green">傳</font><font color= "blue">的</font><font color= "indigo">短</font><font color= "purple">片</font></del></font></center></b></a>
+    </body>
+</html>
