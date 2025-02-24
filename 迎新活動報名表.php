3f8e89da59c42c99d8e45f465563d5733bf553ac
unknown <a0919458922@gmail.com>
2025-02-24 19:50:14 +0800

    迎新活動報名表上傳

---
+++ 迎新活動報名表.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>
+            迎新活動報名表
+        </title>
+    <body bgcolor="#E6D6F0">
+        <meta charset = "UTF-8">
+        <h1><B><center>迎新活動報名表</center></B></h1>
+        <hr>
+
+        <form action = "" method="">
+            <h1><b>基本資料(含保險資料)</b></h1>
+            <OL>
+                <H2><OL>姓名：<input type = "text" size = "30"></OL></H2>
+                <H2><OL>學號：<input type = "text" size = "30"></OL></H2>
+                <H2><OL>性別：<input type = "radio" name = "gender" value = "male "size = "30">男 <input type = "radio" name= "gender" value = "female" size = "30">女 <input type = "radio" name= "gender" value = "other" size = "30">其他</OL></H2>
+                <H2><OL>生日：<input type = "date" size = "30"></OL></H2>
+                <H2><OL>身分證：<input type = "text" size = "30"></OL></H2>
+                <H2><OL>飲食：<input type = "radio" name = "Eating" value = "meat" size = "30">葷食 <input type = "radio" name = "Eating" value = "vegetarian" size = "30">素食 其他：<input type = "text" size = "30"></OL></H2>
+            </OL>
+            <h1><b>聯絡資料</b></h1>
+            <OL>
+                <H2><OL>電話：<input type = "text" size = "30"></OL></H2>
+                <H2><OL>Email：<input type = "text" size = "30"></OL></H2>
+                <table border="1" style="font-size: 24px;">
+                    <tr><td></td><td><center>姓名</center></td><td><center>電話</center></td><td><center>關係</center></td></tr>
+                    <tr><td>緊急聯絡人1</td><td><input type = "text" style="font-size: 24px;"></td><td><input type = "text" style="font-size: 24px;"></td><td><input type = "text" style="font-size: 24px;"></td></tr>
+                    <tr><td>緊急聯絡人2</td><td><input type = "text" style="font-size: 24px;"></td><td><input type = "text" style="font-size: 24px;"></td><td><input type = "text" style="font-size: 24px;"></td></tr>
+                    <tr><td>緊急聯絡人3</td><td><input type = "text" style="font-size: 24px;"></td><td><input type = "text" style="font-size: 24px;"></td><td><input type = "text" style="font-size: 24px;"></td></tr>
+                </table>
+            </OL>
+            <center><input type = "submit" value = "送出" style="font-size: 24px;"><input type = "reset" value = "清除"style="font-size: 24px;"></center>
+        </form>
+    </body>
+</html>
